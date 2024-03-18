f5a81155a1fc66c017b402944bfa02e164163f8b
lunatokki <todis369369@naver.com>
2024-03-19 03:08:45 +0900

    modifying about.js

--- src/store/about.js
+++ src/store/about.js
@@ -1,10 +1,10 @@
 import { Store } from '../core/heropy'
 
 export default new Store({
-  photo: 'https://heropy.blog/css/images/logo.png'
+  photo: 'https://heropy.blog/css/images/logo.png',
   name: 'HEROPY / ParkYoungWoong', 
   email: 'todis369369@naver.com',
   blog: 'https://heropy.blog',
-  github: 'https://github.com/ParkYoungWoong'
+  github: 'https://github.com/ParkYoungWoong',
   repository: 'https://github.com/ParkYoungWoong/vanillajs-movie-app'
 })
